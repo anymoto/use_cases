7dddb55a33ae6a20236e51c9a88b001804531c1c
anymoto <ana@castromartinez.com>
2015-12-13 23:23:57 -0600

    Display use cases container

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,6 @@
 class WelcomeController < ApplicationController
   def index
-
+    json_object = JSON.parse(File.read('public/use_cases.json'), object_class: OpenStruct)
+    @use_cases = json_object.empty? ? nil : json_object.use_cases
   end
 end
