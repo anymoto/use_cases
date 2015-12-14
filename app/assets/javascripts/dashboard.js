7867e656f9dc6fcd63a15ae8c8b50c945d4da83d
anymoto <ana@castromartinez.com>
2015-12-14 12:54:19 -0600

    Toggle use cases content

---
+++ app/assets/javascripts/dashboard.js
@@ -0,0 +1,16 @@
+$(document).ready( function() {
+  $('.js-toggle-content').click(function(e) {
+    e.preventDefault();
+    $('.js-use-case-hidden').toggle('slow', function() {
+      if ($(this).is(':visible')) {
+        e.target.innerHTML = 'See less';
+      } else {
+        e.target.innerHTML = 'See more';
+      }
+    });
+  });
+
+  $('.js-upload-button').click(function(e) {
+    e.preventDefault();
+  });
+});
