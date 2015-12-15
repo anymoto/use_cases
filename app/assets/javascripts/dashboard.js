d5445813056f6cc8d82449b82cebfa486c97689a
anymoto <ana@castromartinez.com>
2015-12-14 20:18:54 -0600

    Add text-area events

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -8,6 +8,7 @@ $(document).ready( function() {
         e.target.innerHTML = 'See more';
       }
     });
+    $('.carrot').toggleClass('up');
   });
 
   $('.js-upload-button').click(function(e) {
@@ -17,4 +18,25 @@ $(document).ready( function() {
   Dropzone.options.sampleDropzone = {
     dictDefaultMessage: 'or drop files here'
   }
+
+  $('.submit-form-button').click(function(e) {
+    e.preventDefault();
+    if ($(this).hasClass('disabled')) {
+      return false;
+    }
+  });
+
+  $('.text-area').keyup(function(e) {
+    length = $(this).val().length;
+    if (length > 0 && length < 141) {
+      $('.submit-form-button').removeClass('disabled');
+      $('.text-warning').addClass('hidden');
+    } else {
+      $('.submit-form-button').addClass('disabled');
+    }
+
+    if (length > 140) {
+      $('.text-warning').removeClass('hidden');
+    }
+  });
 });
