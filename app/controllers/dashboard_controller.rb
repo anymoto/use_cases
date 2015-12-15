ba9de53699cfba4100288dbc83d3b0ed092fd4bf
anymoto <ana@castromartinez.com>
2015-12-15 10:47:28 -0600

    Rename Welcome controller

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,4 @@
-class WelcomeController < ApplicationController
+class DashboardController < ApplicationController
   def index
     json_object = JSON.parse(File.read('public/use_cases.json'), object_class: OpenStruct)
     @use_cases = json_object.empty? ? nil : json_object.use_cases
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 Rails.application.routes.draw do
-  root 'welcome#index'
+  root 'dashboard#index'
 end
